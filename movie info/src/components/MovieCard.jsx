2e706172a400522bfa6a393cb6a6d19f8e07c882
atomnoid <aayushs2580@gmail.com>
2026-06-29 00:43:32 +0530

    updated movie card

--- movie info/src/components/MovieCard.jsx
+++ movie info/src/components/MovieCard.jsx
@@ -1,16 +1,23 @@
-function MovieCard({movie}){
-    function onFavoriteClick(){
-        alert("clicked")
-    }
-    return <div className="movie-card">
-        <div className="movie-poster">
-            <img src={movie.url} alt={movie.tittle} />
-            <div className="movie-overlay">
-                <button className="favorite-btn" onClick={onFavoriteClick}>❤︎</button>
-
-            </div>
-
+function MovieCard({ movie }) {
+  function onFavoriteClick() {
+    alert("clicked");
+  }
+  return (
+    <div className="movie-card">
+      <div className="movie-poster">
+        <img src={movie.url} alt={movie.tittle} />
+        <div className="movie-overlay">
+          <button className="favorite-btn" onClick={onFavoriteClick}>
+            ❤︎
+          </button>
         </div>
-
+      </div>
+      <div className="movie-info">
+        <h3>{movie.tittle}</h3>
+        <p>{movie.release_date}</p>
+      </div>
     </div>
+  );
 }
+
+export default MovieCard
