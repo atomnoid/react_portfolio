d180b11d25a7c63201e10f22c624c81e6db1a50a
atomnoid <aayushs2580@gmail.com>
2026-06-29 13:36:48 +0530

    removed the movie card component

--- movie info/src/App.jsx
+++ movie info/src/App.jsx
@@ -1,16 +1,10 @@
 import "./App.css";
-import MovieCard from "./components/MovieCard";
 
 // This is a component
 function App() {
-  const movieNumber = 1; 
+
   return (
     <>
-    {movieNumber === 1 ? (
-      <MovieCard movie={{title: "My film", release_date:"2025"}} />
-    ) : ( 
-    <MovieCard movie={{title: "your film", release_date:"2027"}} /> 
-    )}
    
     </>
   );
