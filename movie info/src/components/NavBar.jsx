2dd0365f3e1a4a039b8c8a6e038fd505daa3a0c0
atomnoid <aayushs2580@gmail.com>
2026-06-30 01:36:33 +0530

    nav bar component

--- movie info/src/components/NavBar.jsx
+++ movie info/src/components/NavBar.jsx
@@ -1,15 +1,21 @@
 import { Link } from "react-router-dom";
 
-function NavBar (){
-    return <nav className="navabar">
-        <div className="navbar-brand">
-            <Link to="/">Movie Info</Link>
-        </div>
-        <div className="navbar-links">
-            <link to="/" className="nav-link">Home</link>
-             <link to="/" className="nav-link">Favorites</link>
-        </div>
+function NavBar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-brand">
+        <Link to="/">Movie Info</Link>
+      </div>
+      <div className="navbar-links">
+        <Link to="/" className="nav-link">
+          Home
+        </Link>
+        <Link to="/Favorites" className="nav-link">
+          Favorites
+        </Link>
+      </div>
     </nav>
+  );
 }
 
-export default NavBar
+export default NavBar;
