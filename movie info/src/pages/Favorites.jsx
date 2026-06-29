6461163a40429584d801f9fb29c70477df356cc2
atomnoid <aayushs2580@gmail.com>
2026-06-29 23:57:06 +0530

    created favorites page

---
+++ movie info/src/pages/Favorites.jsx
@@ -0,0 +1,11 @@
+function favorite() {
+    return(
+        <div>
+            <h3>Hey this is the favorite page</h3>
+            <p>This is where you can see your favorite movies</p>
+        </div>
+    )
+
+}
+
+export default favorite
