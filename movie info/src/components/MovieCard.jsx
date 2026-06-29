9c79d81186a66e62642c193b505d9a80df7777e5
atomnoid <aayushs2580@gmail.com>
2026-06-29 13:19:12 +0530

    fixed typo

--- movie info/src/components/MovieCard.jsx
+++ movie info/src/components/MovieCard.jsx
@@ -5,7 +5,7 @@ function MovieCard({ movie }) {
   return (
     <div className="movie-card">
       <div className="movie-poster">
-        <img src={movie.url} alt={movie.tittle} />
+        <img src={movie.url} alt={movie.title} />
         <div className="movie-overlay">
           <button className="favorite-btn" onClick={onFavoriteClick}>
             ❤︎
