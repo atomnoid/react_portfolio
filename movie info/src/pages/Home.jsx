d27d40133f80af98a84f173c874ce3f6f38f1e2f
atomnoid <aayushs2580@gmail.com>
2026-06-29 23:57:49 +0530

    added new  functions

--- movie info/src/pages/Home.jsx
+++ movie info/src/pages/Home.jsx
@@ -1,37 +1,35 @@
 import MovieCard from "../components/MovieCard";
 import { useState } from "react";
 function Home() {
-    const [seachQuery, setSearchQuery] = useState("");
-    const movies = [
-        { id: 1, title: "my movie", release_date: "2020" },
-        { id: 2, title: "Your movie", release_date: "2024" },
-        { id: 3, title: "Their movie", release_date: "1997" },
-    ];
+  const [seachQuery, setSearchQuery] = useState("");
+  const movies = [
+    { id: 1, title: "my movie", release_date: "2020" },
+    { id: 2, title: "Your movie", release_date: "2024" },
+    { id: 3, title: "Their movie", release_date: "1997" },
+  ];
 
+  const handleSearch = () => {};
 
-    const handleSearch = () => {
-
-    }
-
-
-    return (
-        <div className="home">
-            <form onSubmit={handleSearch } className="search-form">
-                <input type="text" 
-                 placeholder="search for movies..."
-                 className="search-input"
-                 value={seachQuery}
-                 onChange={(e) => setSearchQuery(e.target.value)}
-                />
-                <button type="submit" className="search-button">Search</button>
-            </form>
-            <div className="movies-grid">
-                {movies.map((movie) => 
-                movie.title.toLowerCase().startsWith(seachQuery) && (
-                    <MovieCard movie={movie} key={movie.id} />
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div className="home">
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="search for movies..."
+          className="search-input"
+          value={seachQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
+      <div className="movies-grid">
+        {movies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id} />
+        ))}
+      </div>
+    </div>
+  );
 }
 export default Home;
