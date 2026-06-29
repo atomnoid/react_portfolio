7b53e62bec2b3ddc184723464cb3bbc1a88fef7d
atomnoid <aayushs2580@gmail.com>
2026-06-30 00:08:14 +0530

    main page routing

--- movie info/src/App.jsx
+++ movie info/src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.css";
 import Home from "./pages/Home";
+import {Routes, Route} from "react-router-dom"
 // This is a component
 function App() {
 
   return (
-    <>
-   <Home />
-    </>
+    <main className="main-content">
+      <Routes>
+        <Route path="/" element={<Home />}/>
+      </Routes>
+
+    </main>
   );
 }
 
