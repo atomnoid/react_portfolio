0cd2afeca82c25130d5bbe2b9effaa904456e3d9
atomnoid <aayushs2580@gmail.com>
2026-06-30 21:32:49 +0530

    added use effect

--- movie info/src/App.jsx
+++ movie info/src/App.jsx
@@ -1,21 +1,21 @@
 import "./css/App.css";
 import Home from "./pages/Home";
-import {Routes, Route} from "react-router-dom"
 import Favorites from "./pages/Favorites";
+import { MovieProvider } from "./contexts/MovieContext";
+import { Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
-// This is a component
-function App() {
 
-  return ( 
-    <div>
-        <NavBar />
-    <main className="main-content">
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/Favorites" element={<Favorites />} />
-      </Routes>
+function App() {
+  return (
+    <MovieProvider>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
       </main>
-    </div>
+    </MovieProvider>
   );
 }
 
